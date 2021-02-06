688732323eee3701841716ebd616beba5463c131
tommasosansone91 <tommaso.sansone@mail.polimi.it>
2021-02-06 18:31:05 +0100

    billing profile mnager

--- billing/models.py
+++ billing/models.py
@@ -4,9 +4,37 @@ from django.conf import settings
 # from python_ecommerce.utils import unique_order_id_generator
 from django.db.models.signals import pre_save, post_save
 
+from accounts.models import GuestEmail
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+
+
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user=request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+
+        if user.is_authenticated:
+            obj, created = self.model.objects.get_or_create(
+                user=user, 
+                email=user.email
+                )
+
+        elif guest_email_id is not None:
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                email=guest_email_obj.email
+                )
+
+        else:
+            print("nè guest nè user")
+
+        return obj
+
 
 class BillingProfile(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE) #OneToOneField
@@ -15,6 +43,8 @@ class BillingProfile(models.Model):
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     # customer is in stripe or brwwin
+
+    objects = BillingProfileManager()
 
     def __str__(self):
         return self.email
