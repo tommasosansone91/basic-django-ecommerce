4fde307b4f419b8e5974ab0808cbfc64d2b4d0a5
Tommaso Sansone <tommaso.sansone91@virgilio.it>
2023-12-08 19:52:10 +0100

    end of lesson 121 - Create the User Model Manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser
+    AbstractBaseUser, BaseUserManager
 )
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):  # only required fileds are put as input here
+        
+        if not email:
+            raise ValueError("Users must have an email address")  # indicatd y the docs
+        if not password:
+            raise ValueError("Users must have a password")  # indicatd y the docs
+        
+        user_obj = self.model(
+            email = self.normalize_email(email)
+        )
+
+        user_obj.set_passsword(password)  # setter per passwod
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+
+        return user_obj
+    
+    # metodi per definire tpi di utente "predefiniti", con attributi uguale ad un valore prefissato
+    
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password = password,
+            is_staff = True
+        )
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password = password,
+            is_admin = True
+        )
+        return user
 
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
@@ -18,6 +57,8 @@ class User(AbstractBaseUser):
     # email and password are required
 
     REQUIRED_FIELDS = []
+
+    objects = UserManager
 
     def __str__(self):
         return self.email
